a406d43de3eea60765252f3439b318251fa3165f
girvel <widauka@ya.ru>
2025-12-12 22:39:43 +0500

    Finishing tasks

--- static/js/index.js
+++ static/js/index.js
@@ -7,14 +7,20 @@ console.log(response.data);
 
 tasks.replaceChildren()
 for (const entry of response.data) {
-  const div = document.createElement("div")
-    const cb = document.createElement("input")
-    cb.type = "checkbox"
-    div.appendChild(cb)
+  const div = document.createElement("div");
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.addEventListener("change", async () => {
+      const response = await Axios.post(`/api/tasks/${entry.id}`);
+      if (response.data.status == "OK") {
+        tasks.removeChild(div);
+      }
+    });
+    div.appendChild(cb);
 
-    const label = document.createElement("label")
-    label.textContent = entry.text
-    div.appendChild(label)
-  tasks.appendChild(div)
+    const label = document.createElement("label");
+    label.textContent = entry.text;
+    div.appendChild(label);
+  tasks.appendChild(div);
 }
 
